4a951632d63306f8b481fb8921fc4dfdbd16a8fc
Daniel Alviso <dan.alviso@yahoo.com>
2018-10-08 16:26:55 -0700

    Added separate app file and added functionality to calculate status

--- hangman.js
+++ hangman.js
@@ -2,6 +2,21 @@ const Hangman = function(word, remainingGuesses) {
   this.word = word.toLowerCase().split("");
   this.remainingGuesses = remainingGuesses;
   this.guessedLetters = [];
+  this.status = "playing";
+};
+
+Hangman.prototype.calculateStatus = function() {
+  const finished = this.word.every(
+    letter => this.guessedLetters.includes(letter) || letter === " "
+  );
+
+  if (this.remainingGuesses === 0) {
+    this.status = "failed";
+  } else if (finished) {
+    this.status = "finished";
+  } else {
+    this.status = "playing";
+  }
 };
 
 Hangman.prototype.getPuzzle = function() {
@@ -30,18 +45,6 @@ Hangman.prototype.makeGuess = function(guess) {
   if (isUnique && isBadGuess) {
     this.remainingGuesses--;
   }
+
+  this.calculateStatus();
 };
-
-const game1 = new Hangman("Cat", 2);
-
-console.log(game1.getPuzzle());
-console.log(game1.remainingGuesses);
-
-window.addEventListener("keypress", function(e) {
-  const guess = String.fromCharCode(e.charCode);
-  if (guess.match(/^[a-z0-9 ]$/i)) {
-    game1.makeGuess(guess);
-    console.log(game1.getPuzzle());
-    console.log(game1.remainingGuesses);
-  }
-});
