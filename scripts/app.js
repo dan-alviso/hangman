8a2157163510743531014b3bc390e8c6db14ee8c
Daniel Alviso <dan.alviso@yahoo.com>
2018-10-09 13:45:53 -0700

    Added css styling and spacing with javascript

--- scripts/app.js
+++ scripts/app.js
@@ -2,14 +2,23 @@ const puzzleEl = document.querySelector("#puzzle");
 const guessesEl = document.querySelector("#guesses");
 const game1 = new Hangman("Cat", 2);
 
-puzzleEl.textContent = game1.puzzle;
-guessesEl.textContent = game1.statusMessage;
+const render = () => {
+  puzzleEl.innerHTML = "";
+  guessesEl.textContent = game1.statusMessage;
+
+  game1.puzzle.split("").forEach(letter => {
+    const letterEl = document.createElement("span");
+    letterEl.textContent = letter;
+    puzzleEl.appendChild(letterEl);
+  });
+};
 
 window.addEventListener("keypress", function(e) {
   const guess = String.fromCharCode(e.charCode);
   if (guess.match(/^[a-z0-9 ]$/i)) {
     game1.makeGuess(guess);
-    puzzleEl.textContent = game1.puzzle;
-    guessesEl.textContent = game1.statusMessage;
+    render();
   }
 });
+
+render();
