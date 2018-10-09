5eee22bfa849f1299d121bf0566744c1756f50c1
Daniel Alviso <dan.alviso@yahoo.com>
2018-10-09 12:16:21 -0700

    Implemented getters for the puzzle and statusMessage methods

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const puzzleEl = document.querySelector("#puzzle");
 const guessesEl = document.querySelector("#guesses");
 const game1 = new Hangman("Cat", 2);
 
-puzzleEl.textContent = game1.getPuzzle();
-guessesEl.textContent = game1.getStatusMessage();
+puzzleEl.textContent = game1.puzzle;
+guessesEl.textContent = game1.statusMessage;
 
 window.addEventListener("keypress", function(e) {
   const guess = String.fromCharCode(e.charCode);
   if (guess.match(/^[a-z0-9 ]$/i)) {
     game1.makeGuess(guess);
-    puzzleEl.textContent = game1.getPuzzle();
-    guessesEl.textContent = game1.getStatusMessage();
+    puzzleEl.textContent = game1.puzzle;
+    guessesEl.textContent = game1.statusMessage;
   }
 });
